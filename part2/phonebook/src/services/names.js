c34ade7f6ba3203422c725d36f48b59b115a00cb
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2024-02-07 08:57:46 +0800

    part2 phonebook

--- part2/phonebook/src/services/names.js
+++ part2/phonebook/src/services/names.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3003'
+const baseUrl = 'http://localhost:3001'
 
 const getAll = () => {
   const request = axios.get(`${baseUrl}/api/persons`)
